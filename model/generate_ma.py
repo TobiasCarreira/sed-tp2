627d64a1a0114c252d00b47014d3b5ea60450767
Tobias Carreira Munich <tobiascarreira@gmail.com>
2021-11-12 22:59:10 -0300

    Prisioneros random

--- model/generate_ma.py
+++ model/generate_ma.py
@@ -1,4 +1,5 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from random import randint, sample
 
 env = Environment(
     loader=FileSystemLoader("."),
@@ -10,7 +11,6 @@ sentences = [100, 1000, 5000]
 delay = [(0,5), (0,10), (5,10), (10,20)]
 betrayal_probas = ['randInt(4) = 0', 'randInt(1) = 0', 'randInt(4) != 0'] # [1/5, 1/2, 4/5]
 prisioners_number = 5
-# TODO: generar varios 'prisioneros.val' aleatorios
 
 with open("prisioneros.ma", "w") as f:
     f.write(template.render(prisioners=list(range(prisioners_number)),
@@ -19,6 +19,15 @@ with open("prisioneros.ma", "w") as f:
                             initial_values_file='prisioneros.val',
                             added_sentence=100,
                             betrayal_proba=betrayal_probas[1]))
+
+
+def random_prisoners(width, height, quantity, sentence=5000):
+    positions = sample(list(np.ndindex((width, height))), quantity)
+    return [
+        dict(x=x, y=y, direction=randint(1,4), sentence=sentence)
+        for x,y in positions
+    ]
+
 
 template = env.get_template("prisioneros.val.j2")
 with open("prisioneros.val", "w") as f:
