4748525ad59e9221618b5c68579cacf0db8cd2df
Valentin Paz Marcolla <valentin.paz@jampp.com>
2021-11-15 21:22:34 -0300

    datos

--- model/generate_ma.py
+++ model/generate_ma.py
@@ -1,41 +1,52 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-from random import randint, sample
+from random import randint, sample, normalvariate
 import numpy as np
 
 env = Environment(
     loader=FileSystemLoader("."),
     autoescape=select_autoescape(),
 )
-template = env.get_template("prisioneros.ma.j2")
+ma_template = env.get_template("prisioneros.ma.j2")
 
 sentences = [100, 1000, 5000]
-delay = [(0,5), (0,10), (5,10), (10,20)]
+delays = [(0,5), (0,10), (5,10), (10,20)]
 betrayal_probas = ['randInt(4) = 0', 'randInt(1) = 0', 'randInt(4) != 0'] # [1/5, 1/2, 4/5]
-prisioners_number = 5
-
-with open("prisioneros.ma", "w") as f:
-    f.write(template.render(prisioners=list(range(prisioners_number)),
-                            max_delay=4,
-                            min_delay=0,
-                            initial_values_file='prisioneros.val',
-                            added_sentence=100,
-                            betrayal_proba=betrayal_probas[1]))
+num_prisioners = 30
+num_runs = 15
+width = 12
 
 
-def random_prisoners(width, height, quantity, sentence=5000):
+def random_prisoners(width, height, quantity, mean_initial_sentence=50, std_initial_sentence=10):
     positions = sample(list(np.ndindex((width, height))), quantity)
     return [
-        dict(x=x, y=y, direction=randint(1,4), sentence=sentence)
+        dict(x=x, y=y, direction=randint(1,4), sentence=100*max(5, int(normalvariate(mean_initial_sentence, std_initial_sentence))))
         for x,y in positions
     ]
 
+prisioners_template = env.get_template("prisioneros.val.j2")
+for i in range(num_runs):
+    with open(f"prisioneros.val.{i}", "w") as f:
+        f.write(prisioners_template.render(prisioners=random_prisoners(width, width, num_prisioners)))
 
-template = env.get_template("prisioneros.val.j2")
-with open("prisioneros.val", "w") as f:
-    f.write(template.render(prisioners=[
-        dict(x=0,y=0, direction=1, denounced_by_2=4),
-        dict(x=0,y=1, direction=3),
-        dict(x=5,y=5, direction=1, sentence=2000, denounced_by_2=0),
-        dict(x=5,y=6, direction=3),
-        dict(x=6,y=6, direction=4,denounced_by_4=0),
-    ]))
+suffix=0
+suffixes={}
+for sentence in sentences:
+    for delay in delays:
+        for betrayal in betrayal_probas:
+            min_delay, max_delay = delay
+            for i in range(num_runs):
+
+                with open(f"prisioneros.ma.{suffix}", "w") as f:
+                    f.write(ma_template.render(prisioners=list(range(num_prisioners)),
+                                            max_delay=max_delay,
+                                            min_delay=min_delay,
+                                            initial_values_file=f"prisioneros.val.{i}",
+                                            added_sentence=sentence,
+                                            betrayal_proba=betrayal,
+                                            width=width,
+                                            height=width))
+                suffixes[suffix] = (sentence, delay, betrayal)
+                suffix +=1
+
+print(suffixes)
+
